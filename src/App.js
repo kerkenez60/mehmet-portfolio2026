1bf3b61e5bc47f3f6c48cab89378d2078b767a21
kerkenez60 <mehmetozen188@gmail.com>
2026-05-19 05:26:15 +0300

    Google doğrulama dosyası eklendi

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+<meta name="google-site-verification" content="fgs-NbEN4r4D0moIGxhglzQIIxoW9jcxJwphdsaALls" />
 
 function App() {
   return (
